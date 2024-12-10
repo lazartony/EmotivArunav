cbe880e12e66988946dc0e729945b33319b051e6
lazaraus <lazartony00@gmail.com>
2024-12-10 21:47:52 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,241 @@
+from dotenv import load_dotenv, dotenv_values
+from cortex import Cortex
+from pythonosc import udp_client
+
+
+class Subcribe():
+    """
+    A class to subscribe data stream.
+
+    Attributes
+    ----------
+    c : Cortex
+        Cortex communicate with Emotiv Cortex Service
+
+    Methods
+    -------
+    start():
+        start data subscribing process.
+    sub(streams):
+        To subscribe to one or more data streams.
+    on_new_data_labels(*args, **kwargs):
+        To handle data labels of subscribed data 
+    on_new_eeg_data(*args, **kwargs):
+        To handle eeg data emitted from Cortex
+    on_new_mot_data(*args, **kwargs):
+        To handle motion data emitted from Cortex
+    on_new_dev_data(*args, **kwargs):
+        To handle device information data emitted from Cortex
+    on_new_met_data(*args, **kwargs):
+        To handle performance metrics data emitted from Cortex
+    on_new_pow_data(*args, **kwargs):
+        To handle band power data emitted from Cortex
+    """
+
+    def __init__(self, app_client_id, app_client_secret, osc_ip, osc_port, **kwargs):
+
+        self.osc_client = udp_client.SimpleUDPClient(osc_ip, osc_port)
+        self.c = Cortex(app_client_id, app_client_secret,
+                        debug_mode=True, **kwargs)
+        self.c.bind(create_session_done=self.on_create_session_done)
+        self.c.bind(new_data_labels=self.on_new_data_labels)
+        self.c.bind(new_eeg_data=self.on_new_eeg_data)
+        self.c.bind(new_mot_data=self.on_new_mot_data)
+        self.c.bind(new_dev_data=self.on_new_dev_data)
+        self.c.bind(new_met_data=self.on_new_met_data)
+        self.c.bind(new_pow_data=self.on_new_pow_data)
+        self.c.bind(inform_error=self.on_inform_error)
+
+    def start(self, streams, headsetId=''):
+        """
+        To start data subscribing process as below workflow
+        (1)check access right -> authorize -> connect headset->create session
+        (2) subscribe streams data
+        'eeg': EEG
+        'mot' : Motion
+        'dev' : Device information
+        'met' : Performance metric
+        'pow' : Band power
+        'eq' : EEQ Quality
+
+        Parameters
+        ----------
+        streams : list, required
+            list of streams. For example, ['eeg', 'mot']
+        headsetId: string , optional
+             id of wanted headet which you want to work with it.
+             If the headsetId is empty, the first headset in list will be set as wanted headset
+        Returns
+        -------
+        None
+        """
+        self.streams = streams
+
+        if headsetId != '':
+            self.c.set_wanted_headset(headsetId)
+
+        self.c.open()
+
+    def sub(self, streams):
+        """
+        To subscribe to one or more data streams
+        'eeg': EEG
+        'mot' : Motion
+        'dev' : Device information
+        'met' : Performance metric
+        'pow' : Band power
+
+        Parameters
+        ----------
+        streams : list, required
+            list of streams. For example, ['eeg', 'mot']
+
+        Returns
+        -------
+        None
+        """
+        self.c.sub_request(streams)
+
+    def unsub(self, streams):
+        """
+        To unsubscribe to one or more data streams
+        'eeg': EEG
+        'mot' : Motion
+        'dev' : Device information
+        'met' : Performance metric
+        'pow' : Band power
+
+        Parameters
+        ----------
+        streams : list, required
+            list of streams. For example, ['eeg', 'mot']
+
+        Returns
+        -------
+        None
+        """
+        self.c.unsub_request(streams)
+
+    def on_new_data_labels(self, *args, **kwargs):
+        """
+        To handle data labels of subscribed data 
+        Returns
+        -------
+        data: list  
+              array of data labels
+        name: stream name
+        For example:
+            eeg: ["COUNTER","INTERPOLATED", "AF3", "T7", "Pz", "T8", "AF4", "RAW_CQ", "MARKER_HARDWARE"]
+            motion: ['COUNTER_MEMS', 'INTERPOLATED_MEMS', 'Q0', 'Q1', 'Q2', 'Q3', 'ACCX', 'ACCY', 'ACCZ', 'MAGX', 'MAGY', 'MAGZ']
+            dev: ['AF3', 'T7', 'Pz', 'T8', 'AF4', 'OVERALL']
+            met : ['eng.isActive', 'eng', 'exc.isActive', 'exc', 'lex', 'str.isActive', 'str', 'rel.isActive', 'rel', 'int.isActive', 'int', 'foc.isActive', 'foc']
+            pow: ['AF3/theta', 'AF3/alpha', 'AF3/betaL', 'AF3/betaH', 'AF3/gamma', 'T7/theta', 'T7/alpha', 'T7/betaL', 'T7/betaH', 'T7/gamma', 'Pz/theta', 'Pz/alpha', 'Pz/betaL', 'Pz/betaH', 'Pz/gamma', 'T8/theta', 'T8/alpha', 'T8/betaL', 'T8/betaH', 'T8/gamma', 'AF4/theta', 'AF4/alpha', 'AF4/betaL', 'AF4/betaH', 'AF4/gamma']
+        """
+        data = kwargs.get('data')
+        stream_name = data['streamName']
+        stream_labels = data['labels']
+        print('{} labels are : {}'.format(stream_name, stream_labels))
+        if stream_name == 'pow':
+            self.pow_labels = stream_labels
+        elif stream_name == 'met':
+            self.met_labels = stream_labels
+
+    def on_new_eeg_data(self, *args, **kwargs):
+        """
+        To handle eeg data emitted from Cortex
+
+        Returns
+        -------
+        data: dictionary
+             The values in the array eeg match the labels in the array labels return at on_new_data_labels
+        For example:
+           {'eeg': [99, 0, 4291.795, 4371.795, 4078.461, 4036.41, 4231.795, 0.0, 0], 'time': 1627457774.5166}
+        """
+        data = kwargs.get('data')
+        print('eeg data: {}'.format(data))
+
+    def on_new_mot_data(self, *args, **kwargs):
+        """
+        To handle motion data emitted from Cortex
+
+        Returns
+        -------
+        data: dictionary
+             The values in the array motion match the labels in the array labels return at on_new_data_labels
+        For example: {'mot': [33, 0, 0.493859, 0.40625, 0.46875, -0.609375, 0.968765, 0.187503, -0.250004, -76.563667, -19.584995, 38.281834], 'time': 1627457508.2588}
+        """
+        data = kwargs.get('data')
+        print('motion data: {}'.format(data))
+
+    def on_new_dev_data(self, *args, **kwargs):
+        """
+        To handle dev data emitted from Cortex
+
+        Returns
+        -------
+        data: dictionary
+             The values in the array dev match the labels in the array labels return at on_new_data_labels
+        For example:  {'signal': 1.0, 'dev': [4, 4, 4, 4, 4, 100], 'batteryPercent': 80, 'time': 1627459265.4463}
+        """
+        data = kwargs.get('data')
+        print('dev data: {}'.format(data))
+
+    def on_new_met_data(self, *args, **kwargs):
+        """
+        To handle performance metrics data emitted from Cortex
+
+        Returns
+        -------
+        data: dictionary
+             The values in the array met match the labels in the array labels return at on_new_data_labels
+        For example: {'met': [True, 0.5, True, 0.5, 0.0, True, 0.5, True, 0.5, True, 0.5, True, 0.5], 'time': 1627459390.4229}
+        """
+        data = kwargs.get('data')
+        print('pm data: {}'.format(data))
+        # self.osc_client.send_message('/met', data['met'])
+
+    def on_new_pow_data(self, *args, **kwargs):
+        """
+        To handle band power data emitted from Cortex
+
+        Returns
+        -------
+        data: dictionary
+             The values in the array pow match the labels in the array labels return at on_new_data_labels
+        For example: {'pow': [5.251, 4.691, 3.195, 1.193, 0.282, 0.636, 0.929, 0.833, 0.347, 0.337, 7.863, 3.122, 2.243, 0.787, 0.496, 5.723, 2.87, 3.099, 0.91, 0.516, 5.783, 4.818, 2.393, 1.278, 0.213], 'time': 1627459390.1729}
+        """
+        data = kwargs.get('data')
+        # print('pow data: {}'.format(data))
+        # self.osc_client.send_message('/pow', data['pow'])
+        sum_pow = sum(data['pow'])
+        sum_theta_pow = data['pow'][0] + data['pow'][5]
+        theta_proportion = sum_theta_pow / sum_pow
+        self.osc_client.send_message('/theta_proportion', theta_proportion)
+        print('theta_proportion: {}'.format(theta_proportion))
+
+    # callbacks functions
+
+    def on_create_session_done(self, *args, **kwargs):
+        print('on_create_session_done')
+
+        # subribe data
+        self.sub(self.streams)
+
+    def on_inform_error(self, *args, **kwargs):
+        error_data = kwargs.get('error_data')
+        print(error_data)
+
+    def on_close(self):
+        print('close cortex')
+        self.c.close()
+        self.osc_client.close()
+
+
+load_dotenv()
+
+osc_ip = dotenv_values('creds.env')['osc_ip']
+osc_port = int(dotenv_values('creds.env')['osc_port'])
+client_id = dotenv_values('creds.env')['client_id']
+client_secret = dotenv_values('creds.env')['client_secret']
+sub = Subcribe(client_id, client_secret, osc_ip, osc_port)
+sub.start(['met', 'pow'])
